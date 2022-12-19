f07817e5bad30322175b9806e1787a33f37655d7
mayachiu <mayachiu5g@gmail.com>
2022-12-19 10:30:59 +0900

    Initial Commit

---
+++ Yumemi-iOS-Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yumemi-iOS-Training
+//
+//  Created by 内山 和輝 on 2022/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
