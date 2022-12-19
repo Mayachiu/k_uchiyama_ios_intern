ecbf06a85932df6c4847e2972c34aee860871c6f
mayachiu <mayachiu5g@gmail.com>
2022-12-19 13:28:05 +0900

    ボタンタップで画像を表示させる

--- Yumemi-iOS-Training/ViewController.swift
+++ Yumemi-iOS-Training/ViewController.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
+import YumemiWeather
 
 final class ViewController: UIViewController {
+    @IBOutlet private weak var weatherImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction private func reloadButtonPressed(_ sender: Any) {
+        switch YumemiWeather.fetchWeatherCondition() {
+        case "sunny":
+            weatherImageView.image = UIImage(named: "sunny")?.withRenderingMode(.alwaysTemplate)
+            weatherImageView.tintColor = .red
+        case "rainy":
+            weatherImageView.image = UIImage(named: "rainy")?.withRenderingMode(.alwaysTemplate)
+            weatherImageView.tintColor = .blue
+        case "cloudy":
+            weatherImageView.image = UIImage(named: "cloudy")?.withRenderingMode(.alwaysTemplate)
+            weatherImageView.tintColor = .gray
+        default:
+            weatherImageView.image = UIImage(systemName: "questionmark.circle")
+        }
+    }
 }
 
